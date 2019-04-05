4d216a5ba4cf7e6207b9baf917d9f9659ca73d9c
maov <marcusoverheu@gmail.com>
2019-04-05 11:24:41 +0200

    lots of flatMap and type fiddling

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -17,22 +17,23 @@ object RNG {
     }
   }
 
-  type Rand[+A] = RNG => (A, RNG)
 
-  val int: Rand[Int] = _.nextInt
-
-  def unit[A](a: A): Rand[A] =
-    rng => (a, rng)
-
-  def map[A,B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
-
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
-
-  def double(rng: RNG): (Double, RNG) = ???
+//okey i'm lazy in theory to keep the function random one should keep generating untill != Int.minValue 
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val n = rng.nextInt
+    if(n._1 < 0){
+      if(n._1 == Int.MinValue)  (0, n._2)
+      else  (n._1 * -1, n._2)
+    }else
+      n
+  }
+//lazy here again
+  def double(rng: RNG): (Double, RNG) = {
+    val n = nonNegativeInt(rng)
+    val v = if(n._1 == Int.MaxValue) (Int.MaxValue -1).toDouble else n._1.toDouble
+    var r =   v / (Int.MaxValue ).toDouble
+    (r, n._2)
+  }
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = ???
 
@@ -40,22 +41,79 @@ object RNG {
 
   def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = 
+    {
+    def subF(c: Int, l: List[Int], r: RNG) : (List[Int], RNG) =
+      if(count > 0) {
+        val t2 = r.nextInt 
+        subF(count - 1, t2._1 :: l, t2._2)
+    } else (l,r)
+    subF(count, List(), rng)
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def doubleMap : Rand[Double] = map(int)(n => {
+    val v = if(n == Int.MaxValue) (Int.MaxValue -1).toDouble else n.toDouble
+    v / (Int.MaxValue ).toDouble
+  })
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng =>
+  {
+    val (a, rng1) = ra(rng)
+    val (b, rng2) = rb(rng)
+    (f(a,b), rng2)
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = 
+  fs match {
+    // Rand[A], Rand[List[A]] => Rand[List[A]]
+    // A, List[A] => List[A] , gives Rand[List[C]] in map2
+    case h :: t => map2(h, sequence(t))((a,b) => a :: b)
+    case _ => unit(List[A]())
+  }
+
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A,B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, rng2) = f(rng)
+    val b = g(a)
+    b(rng2)
+  }
+
+  def mapf[A,B](s: Rand[A])(f: A => B) : Rand[B] = flatMap(s)(x => unit(f(x)))
+
+  def map2f[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B) => C): Rand[C] = flatMap(ra)(
+    a => flatMap(rb)(b => unit(f(a,b)))
+  )
 }
-
+//type Rand[A] = State[RNG, A]
 case class State[S,+A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    ???
+  def map[B](f: A => B): State[S, B] = State(s => {
+    val (a,s2) = run(s)
+    (f(a),s2)
+  })
+//    ???
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    flatMap(a => sb.map(b => f(a,b) ))
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(
+    s => {
+      val (a, s2) = run(s)
+      f(a).run(s2)
+      }
+  )
+    
 }
 
 sealed trait Input
